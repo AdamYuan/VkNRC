02548e635cbf456ec6953ca10b65df8da2cc8424
AdamYuan <y13916619121@126.com>
2024-02-23 22:11:32 +0800

    hide symbols

---
+++ src/VkNRCResource.cpp
@@ -0,0 +1,5 @@
+//
+// Created by adamyuan on 2/23/24.
+//
+
+#include "VkNRCResource.hpp"
---
+++ src/VkNRCResource.hpp
@@ -0,0 +1,13 @@
+//
+// Created by adamyuan on 2/23/24.
+//
+
+#pragma once
+#ifndef VKNRC_VKNRCRESOURCE_HPP
+#define VKNRC_VKNRCRESOURCE_HPP
+
+class VkNRCResource {
+
+};
+
+#endif // VKNRC_VKNRCRESOURCE_HPP
